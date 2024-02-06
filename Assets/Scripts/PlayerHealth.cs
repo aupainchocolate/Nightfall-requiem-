2068fedfd0c271757b033546d87b1166e5b81e8c
Sara Guellil <sara.guellil@learnet.se>
2024-02-06 10:53:53 +0100

    more scripts >:)

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CharacterHealth : MonoBehaviour
 {
+    //keeps track of player's current health
+    public int health; 
+
+    // How much health you have when you have full health
+    public int MaxHealth = 10; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       health = MaxHealth; 
     }
 
-    // Update is called once per frame
-    void Update()
+    //Will be called everytime player takes damage
+    public void TakeDamage(int amount)
     {
-        
+        health -= amount;
+        if (health < 0)
+        {
+            Destroy (gameObject);
+        }
     }
 }
--- Assets/Scripts/Sceneloader.cs
+++ Assets/Scripts/Sceneloader.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sceneloader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Method to load a scene asynchronously by name
+    public void LoadScene(string sceneName)
     {
-        
+        // Use SceneManager to load the scene asynchronously by name
+        SceneManager.LoadSceneAsync(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Method to load a scene asynchronously by build index
+    public void LoadScene(int buildIndex)
     {
-        
+        // Use SceneManager to load the scene asynchronously by build index
+        SceneManager.LoadSceneAsync(buildIndex);
     }
 }
