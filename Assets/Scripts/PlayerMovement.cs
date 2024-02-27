ff3a6d233f8ddd48eed6fc6f5e58e72a5d43fc5a
Nadia Yakta <nadia.yakta@learnet.se>
2024-02-27 09:15:53 +0100

    jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,25 +10,31 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
 
+    [SerializeField] Transform groundCheckPosition;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
     }
 
     void Update()
     {
+        Debug.Log(isGrounded);
+        //Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("Han borde hoppa för fan!");
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
         //Horizontal movement
         float moveInput = Input.GetAxis("Horizontal");
         Vector2 moveVelocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
         rb.velocity = moveVelocity;
 
         //Check if the player is grounded
-        isGrounded = Physics2D.OverlapCircle(transform.position, 0.2f, LayerMask.GetMask("Ground"));
+        isGrounded = Physics2D.OverlapCircle(groundCheckPosition.position, 0.2f, LayerMask.GetMask("Ground"));
 
-        //Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+        
     }
 }
